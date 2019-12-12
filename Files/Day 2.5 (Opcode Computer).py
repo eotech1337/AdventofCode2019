74f71f0d3cac31ebc1dccac66660b34a6d606276
eotech1337 <createdvoid@live.com>
2019-12-12 16:43:58 +0000

    191212 Day 2 Complete

--- Files/Day 2.5 (Opcode Computer).py
+++ Files/Day 2.5 (Opcode Computer).py
@@ -4,9 +4,11 @@ ints_raw = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 6, 19, 1, 19, 
             6, 31, 35, 1, 9, 35, 39, 2, 10, 39, 43, 1, 43, 9, 47, 1, 47, 9, 51, 2, 10, 51, 55, 1, 55, 9, 59, 1, 59, 5,
             63, 1, 63, 6, 67, 2, 6, 67, 71, 2, 10, 71, 75, 1, 75, 5, 79, 1, 9, 79, 83, 2, 83, 10, 87, 1, 87, 6, 91, 1,
             13, 91, 95, 2, 10, 95, 99, 1, 99, 6, 103, 2, 13, 103, 107, 1, 107, 2, 111, 1, 111, 9, 0, 99, 2, 14, 0, 0]
+ints_reset = ints_raw[:]
 start_index = 0
 segment_count = int(len(ints_raw) // 4)
 task_counter = 0
+input_required = ()
 
 
 # Opcode 1 Function (addition)
@@ -17,7 +19,6 @@ def opcode_1(segment):
 
     output = ints_raw[index_1] + ints_raw[index_2]
     ints_raw[index_3] = output
-    # print(segment)
 
 
 # Opcode 2 Function (multiplication)
@@ -28,7 +29,6 @@ def opcode_2(segment):
 
     output = ints_raw[index_1] * ints_raw[index_2]
     ints_raw[index_3] = output
-    # print(output)
 
 
 # segment_splitter splits the segment out from the inits_raw list
@@ -45,20 +45,41 @@ def check_opcode(segment):
     elif index_0 == 2:
         opcode_2(segment)
     elif index_0 == 99:
-        print("Program has finished.")
+        print("Program has finished.", index_0)
     else:
-        print('Something went wrong, Encountered an unknown Opcode.')
+        print('Something went wrong, Encountered an unknown Opcode.', index_0)
 
 
-# "before running the program, replace position 1 with the value 12 and replace position 2 with the value 2."
-ints_raw[1] = 12
-ints_raw[2] = 2
+# loop through the two inputs producing the output for each pair of inputs (will add condition for answer later)
+noun = 1
+verb = 1
 
-while task_counter < segment_count:
-    check_opcode(segment_splitter())
-    start_index += 4
-    task_counter += 1
+#  Iterate the noun and verb
+while verb <= 99:
+    noun += 1
+    if noun == 99:
+        noun = 1
+        verb += 1
 
-print(ints_raw[0])
+    #  Insert their values into the ints list
+    ints_raw[1] = noun
+    ints_raw[2] = verb
 
-#loop through the two inputs, until the value of ints_raw[0] = 19690720 
+    #  run the Program
+    while task_counter < segment_count:
+        check_opcode(segment_splitter())
+        start_index += 4
+        task_counter += 1
+
+    #  reset the program counters
+    start_index = 0
+    task_counter = 0
+
+    #  conditional to output the needed program input
+    if ints_raw[0] == 19690720:
+        input_required = [noun, verb]
+    ints_raw = ints_reset.copy()
+
+print(input_required)
+print(100 * input_required[0] + input_required[1])
+
--- Files/List Split counter test.py
+++ Files/List Split counter test.py
@@ -1,14 +1,1 @@
-ints_raw = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2]
-task_counter = 0
-
-
-def segment_splitter():
-    opcodes = ints_raw[int(task_counter):int(task_counter + 4)]
-    return opcodes
-
-'''for n in ints_raw[0::4]:
-    
-    print(n)'''
-
-
-print(segment_splitter())
+print(100 *  + 2)
