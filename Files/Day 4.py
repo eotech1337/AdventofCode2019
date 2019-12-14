8233409ed20150dfdc639c9df916ab9111f49729
eotech1337 <Collins33141379>
2019-12-14 19:38:24 +0000

    Day 4.1 complete

--- Files/Day 4.py
+++ Files/Day 4.py
@@ -1,4 +1,61 @@
-input_range = range(165432,707912)
+input_range = range(165432, 707912)
+# input_range = range(111111, 123456)
+potential_passwords_list = []
 
-for n in input_range:
-    print(n)
+'''
+check ascending
+check at least one double digit
+'''
+
+
+def is_ascending(args):
+    if len(args) <= 6:
+            if args[1] >= args[0]:
+                if args[2] >= args[1]:
+                    if args[3] >= args[2]:
+                        if args[4] >= args[3]:
+                            if args[5] >= args[4]:
+                                return True
+                            else:
+                                return False
+                        else:
+                            return False
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+    else:
+        return False
+
+
+def has_double(args):
+    if len(args) <= 6:
+        if args[1] == args[0]:
+            return True
+        elif args[2] == args[1]:
+            return True
+        elif args[3] == args[2]:
+            return True
+        elif args[4] == args[3]:
+            return True
+        elif args[5] == args[4]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+for num in input_range:
+    potential_password = [int(x) for x in str(num)]
+    if is_ascending(potential_password) is True:
+        if has_double(potential_password) is True:
+            potential_passwords_list.append(potential_password)
+        else:
+            continue
+    else:
+        continue
+
+print(len(potential_passwords_list))
